58e048aaa021c9a257b782ce24bd4eea2706e67b
AbrilVelo <101335470+AbrilVelo@users.noreply.github.com>
2022-04-04 12:08:57 -0300

    tp2
    video 1-4

---
+++ Assets/Scripts/MayorDeEdad.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayorDeEdad : MonoBehaviour
+{
+    public int EdadUsuario;
+    public Text MiTexto;
+    void Start()
+    {
+        
+        if (EdadUsuario >= 18)
+        {
+            MiTexto.text = "Es mayor de edad";
+            Debug.Log("Es mayor de edad");
+        }
+        else
+        {
+            MiTexto.text = "No es mayor de edad";
+            Debug.Log("No es mayor de edad");
+        }
+    }
+
+   
+}
