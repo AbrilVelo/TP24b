85100fed75be4230d7231602481a3cefbbfdbebe
AbrilVelo <101335470+AbrilVelo@users.noreply.github.com>
2022-04-05 17:15:59 -0300

    termine videos
    videos 04-05-06

---
+++ Assets/Scripts/ValidarContraseña.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ValidarContraseña : MonoBehaviour
+{
+    string contraseniaCorrecta;
+    public Text ingresoUsuario;
+    string contraseniaUsuario;
+    public GameObject CartelMsj;
+    public Text textoMsj;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CartelMsj.SetActive(false);
+        contraseniaCorrecta = "12345";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void validarContraseña()
+    {
+        contraseniaUsuario = ingresoUsuario.text;
+        if (contraseniaUsuario == contraseniaCorrecta)
+        {
+            Debug.Log("Bienvenido");
+            CartelMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
+        }
+        else
+        {
+            CartelMsj.SetActive(true);
+            Debug.Log("Contraseña Incorrecta");
+            textoMsj.text = "Contraseña Incorrecta";
+        }
+    }
+}
